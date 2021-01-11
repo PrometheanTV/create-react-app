d5b9a771f170eb0da657900cc625f4ff2ee4d543
Alexander Syed <alexksyed@gmail.com>
2021-01-11 21:26:32 +0100

    override webpack's override of SystemJS

--- packages/react-scripts/config/webpack.config.js
+++ packages/react-scripts/config/webpack.config.js
@@ -368,7 +368,8 @@ module.exports = function (webpackEnv) {
       strictExportPresence: true,
       rules: [
         // Disable require.ensure as it's not a standard language feature.
-        { parser: { requireEnsure: false } },
+        // Disable webpack of overriding SystemJS
+        { parser: { requireEnsure: false, system: false } },
         {
           // "oneOf" will traverse all following loaders until one will
           // match the requirements. When no loader matches it will fall
